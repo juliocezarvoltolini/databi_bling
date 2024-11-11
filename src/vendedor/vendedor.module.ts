e84964cc026d47a558cf2aabec78fe874ce36fd2
Júlio Cezar Voltolini <gitlab@viatechinfo.com.br>
2024-11-10 22:02:07 -0300

    Finalizando vendedores

--- src/vendedor/vendedor.module.ts
+++ src/vendedor/vendedor.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VendedorService } from './vendedor.service';
+import { DataBaseModule } from 'src/data-base/data-base.module';
 
 @Module({
+  imports: [DataBaseModule],
   providers: [VendedorService],
   exports: [VendedorService]
 })
